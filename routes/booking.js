244e6cd125ff7871655fa179815a32ec91c6ac42
Vaishak V K <vaishakhvkv@gmail.com>
2019-01-21 17:17:40 +0530

    Update booking.js

--- routes/booking.js
+++ routes/booking.js
@@ -6,4 +6,21 @@ router.get('/', function(req, res) {
 	res.render("home");
 });
 
+router.get('/offers', function(req, res) {
+	res.render("offer");
+});
+
+router.get('/models', function(req, res) {
+	res.render("model");
+});
+
+router.post('/book', function(req, res) {
+	var pickUpLocation = req.body.location;
+	var startDate = req.body.startDate;
+	var duration = req.body.duration;
+	
+	var carsAvailable = Cars.find({});
+	res.render("book");
+});
+
 module.exports = router;
